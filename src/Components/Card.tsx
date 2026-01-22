851c58b8547b6677d9b7576820957d2c9d95a9c6
Joaquín Navarro <joanavarro23@hotmail.com>
2026-01-22 16:52:52 -0300

    boton y precio card

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,7 +1,10 @@
 import Card from '@mui/material/Card'
 import CardMedia from '@mui/material/CardMedia'
 import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
 import jazminImage from '../assets/jazmin_4.jpg'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
 
 interface ProductCardProps {
     image?: string
@@ -18,8 +21,16 @@ const ProductCard = ({image, title, price}: ProductCardProps) => {
                 title={title || 'Jazmín de la selva'}
             />
             <CardContent>
-                {title || 'Jazmín de la selva'}
+                <Typography variant='h6' align='left'>
+                    {title || 'Jazmín de la selva'}
+                </Typography>
+                <Typography variant='subtitle1' align='left'>
+                    ${price || 30}
+                </Typography>
             </CardContent>
+            <CardActions sx={{justifyContent: 'center'}}>
+                <Button variant='outlined' color='success'>Comprar!!!</Button>
+            </CardActions>
         </Card>
     )
 }
