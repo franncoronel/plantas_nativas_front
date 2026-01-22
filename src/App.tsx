65ccb0997c2b5a1e8a980be01e78a0e8c3092b5c
Joaquín Navarro <joanavarro23@hotmail.com>
2026-01-22 16:36:42 -0300

    componente en app para visualizar

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import theme from './theme.ts'
 import './App.css'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import ProductCard from './Components/Card.tsx'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         <Button variant="contained" size="large">Lorem ipsum</Button>
       </Box>
+      <ProductCard />
     </ThemeProvider>
   )
 }
