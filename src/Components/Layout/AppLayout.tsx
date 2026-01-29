fd60aceeac1b3d384f45f5112586ead21249d160
carlasolrocca <carlasolrocca@gmail.com>
2026-01-29 17:01:27 -0300

    feat: creo AppLayout general para la aplicacion

---
+++ src/Components/Layout/AppLayout.tsx
@@ -0,0 +1,9 @@
+import { HeaderApp } from '../Header.tsx'
+
+export const AppLayout = () => {
+    return (
+        <HeaderApp/>
+    )
+}
+
+//Layout compartido para la mayoria de las vistas, acá estaría el header y el footer
