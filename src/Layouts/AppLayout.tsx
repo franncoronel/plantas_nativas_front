e0c9aed3778d6a5bfeab2c9e594f7fd72467dc1b
Francisco Coronel <ffranncoronel@gmail.com>
2026-02-01 17:15:32 -0300

    feat: Agrego componentes de layout para indicar cómo se deberían renderizar las vistas dependiendo de su estructura

---
+++ src/Layouts/AppLayout.tsx
@@ -0,0 +1,12 @@
+import Box from "@mui/material/Box"
+import Nav from "../Components/Nav"
+import { Outlet } from "react-router"
+
+export const AppLayout = () => {
+    return(
+        <Box>
+            <Outlet />
+            <Nav />
+        </Box>
+    )
+}
