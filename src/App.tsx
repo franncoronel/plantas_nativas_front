5a38e70731732d52a24b4ee7d78b263661f35477
Francisco Coronel <ffranncoronel@gmail.com>
2026-02-01 17:09:27 -0300

    feat: Agrego rutas de la aplicación implementando Layout Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 
 import { ThemeProvider } from '@emotion/react'
 import theme from './theme.ts'
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import Nav from './Components/Nav.tsx'
+import { AppRoutes } from './Routing/AppRoutes.tsx'
 
 const App = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Nav />
+      <AppRoutes />
     </ThemeProvider>
   )
 }
