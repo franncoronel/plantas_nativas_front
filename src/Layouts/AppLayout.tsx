7ad865c209171f9a6512918b2d90888716457395
carlasolrocca <carlasolrocca@gmail.com>
2026-02-02 18:08:49 -0300

    feat: agrego header al layout de la app

--- src/Layouts/AppLayout.tsx
+++ src/Layouts/AppLayout.tsx
@@ -1,11 +1,13 @@
 import Box from "@mui/material/Box"
 import Nav from "../Components/Nav"
+import { HeaderApp } from '../Components/Header'
 import { Outlet } from "react-router"
 
 export const AppLayout = () => {
     return(
         <Box>
             <Outlet />
+            <HeaderApp />
             <Nav />
         </Box>
     )
