de468ae5e03bcc97373ee82bb38346f3cf14f85a
carlasolrocca <carlasolrocca@gmail.com>
2026-02-02 18:09:08 -0300

    chore: corrijo mayuscula en tipado

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -15,7 +15,7 @@ import { PATHS } from '../Routing/paths'
 
 const Nav = () => {
     //TODO: Usar estado o investigar NavLink para renderizar íconos filled en vez de outlined cuando está activo el botón correspondiente
-    const [active, setActive] = useState<Boolean>()
+    const [active, setActive] = useState<boolean>()
 
     return (
         <BottomNavigation>
