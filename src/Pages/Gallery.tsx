90fd3ba88bc11f2ec01e78f8283fba62c9427a9e
Joaquín Navarro <joanavarro23@hotmail.com>
2026-02-19 16:51:49 -0300

    feat: Agrego página de galería

---
+++ src/Pages/Gallery.tsx
@@ -0,0 +1,31 @@
+import Container from "@mui/material/Container"
+import Button from "@mui/material/Button"
+import ProductCard from "@/Components/ProductCard"
+import Grid from "@mui/material/Grid"
+
+export const Gallery = () => {
+    return(
+        <Container>
+            <Grid container spacing={2}>
+                <Grid size={6}>
+                    <Button color="warning" variant="outlined">Filtrar</Button>
+                </Grid>
+                <Grid size={6}>
+                    <Button color="warning" variant="outlined">Ordenar</Button>
+                </Grid>
+                <Grid size={6}>
+                    <ProductCard />
+                </Grid>
+                <Grid size={6}>
+                    <ProductCard />
+                </Grid>
+                <Grid size={6}>
+                    <ProductCard />
+                </Grid>
+                <Grid size={6}>
+                    <ProductCard />
+                </Grid>
+            </Grid>
+        </Container>
+    )   
+}   
