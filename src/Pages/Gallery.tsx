f7a4149d6251dad7ead9a2e49d2fe0134659a89c
Joaquín Navarro <joanavarro23@hotmail.com>
2026-02-19 18:20:17 -0300

    feat: Iconos para los botones en galeria y color negro en texto (capaz hay que hacerlo con el theme)

--- src/Pages/Gallery.tsx
+++ src/Pages/Gallery.tsx
@@ -1,6 +1,8 @@
 import Container from "@mui/material/Container"
 import Button from "@mui/material/Button"
 import ProductCard from "@/Components/ProductCard"
+import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined'
+import SortOutlinedIcon from '@mui/icons-material/SortOutlined'
 import Grid from "@mui/material/Grid"
 
 export const Gallery = () => {
@@ -8,20 +10,14 @@ export const Gallery = () => {
         <Container>
             <Grid container spacing={2}>
                 <Grid container size={12} justifyContent="space-between">
-                    <Button color="warning" variant="outlined">Filtrar</Button>
-                    <Button color="warning" variant="outlined">Ordenar</Button>
+                    <Button variant="text" startIcon={<FilterAltOutlinedIcon />} sx={{ color: "black" }}>Filtrar</Button>
+                    <Button variant="text" startIcon={<SortOutlinedIcon />} sx={{ color: "black" }}>Ordenar</Button> {/* Supongo que el tema del color habría que verlo con el theme */ }
                 </Grid>
-                <Grid size={6}>
-                    <ProductCard />
-                </Grid>
-                <Grid size={6}>
-                    <ProductCard />
-                </Grid>
-                <Grid size={6}>
-                    <ProductCard />
-                </Grid>
-                <Grid size={6}>
-                    <ProductCard />
+                <Grid container size={12} spacing={2} >
+                    <Grid size={6}><ProductCard /></Grid>
+                    <Grid size={6}><ProductCard /></Grid>
+                    <Grid size={6}><ProductCard /></Grid>
+                    <Grid size={6}><ProductCard /></Grid>
                 </Grid>
             </Grid>
         </Container>
